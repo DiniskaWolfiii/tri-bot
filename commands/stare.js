7f2cd1b6d8c4535729fb542830a538c3e64581e0
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2023-06-23 21:30:29 +0200

    Update stare.js

--- commands/stare.js
+++ commands/stare.js
@@ -17,7 +17,16 @@ module.exports = {
         await interaction.deferReply();
         const punchUser = interaction.options.getMember('user');
         let gifs = [
-            `hier sollte eigentlich ein gif sein...`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958253057446039/vibing-vivy-flourite-eyes-song.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958254168952933/anya-forger.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958253804040202/benimaru-stare.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958253426561054/jujutsu-kaisen.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958255687282829/attack-on-titan-stare.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958255091699792/mikasa-staring.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958254668058664/762cc6a148081ddb3b132616bd3f753b.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958257000108173/dabi-my-hero-academia.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958256542912622/kimetsu-no-yaiba-demon-slayer.gif`,
+            `https://cdn.discordapp.com/attachments/1037819436960337990/1080958256119304202/jojo-anime.gif`
 
         ]
         await interaction.editReply(`*${interaction.user} starrt ${punchUser} an :eyes:*`)
