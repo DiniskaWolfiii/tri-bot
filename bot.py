f13055d87a15e08670174ed86a86b8fb984910fe
Wolfiii <54332169+DiniskaWolfiii@users.noreply.github.com>
2024-07-06 17:34:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ import os # default module
 from dotenv import load_dotenv
 import sys
 
+# Tewst
+
 load_dotenv() # load all the variables from the env file
 bot = commands.Bot(intents=discord.Intents.all())
 #bot = commands.Bot(debug_guilds=[1001916230069911703], intents=discord.Intents.all()) # for debugging - Wolfiiis Server
